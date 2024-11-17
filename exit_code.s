43d096f8b060a2f6aa6f6621e1e03436b51d5295
Narendra Nag <narendranag@gmail.com>
2024-11-17 16:50:05 -0600

    Exit Code works

--- exit_code.s
+++ exit_code.s
@@ -1,6 +1,7 @@
 .global _start
-.balign 4
+
+.section __TEXT,__text
 _start:
-	mov w0, #7
-	mov w16, #1
-	svc #0x80
+    mov x0, #42            // Load the exit code (42) into register x0
+    mov x16, #1            // Syscall number for exit is 1 (on ARM64 for macOS)
+    svc #0                 // Trigger syscall (exit)
